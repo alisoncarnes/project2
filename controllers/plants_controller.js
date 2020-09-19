51efa8c56566e38ab1570f8054dcaf74cb6ab8a0
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-09-19 09:31:02 -0700

    pushing files

--- controllers/plants_controller.js
+++ controllers/plants_controller.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Plants = require('../models/plants.js')
 const plants = express.Router()
 
-SHOP PLANTS ROUTE
+// SHOP PLANTS ROUTE
 plants.get('/plants' , (req, res) => {
   res.render('/shop_plants.ejs');
 });
