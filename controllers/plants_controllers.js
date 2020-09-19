5f0576f57d82820c814d8ab849bd008d46caaa82
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-09-19 08:48:37 -0700

    editing shop page

--- controllers/plants_controllers.js
+++ controllers/plants_controllers.js
@@ -2,8 +2,10 @@ const express = require('express')
 const Plants = require('../models/plants.js')
 const plants = express.Router()
 
-
-
+//SHOP PLANTS ROUTE
+app.get('/plants' , (req, res) => {
+  res.render('shop_plants.ejs');
+});
 
 
 module.exports = plants
