0f35b69da492c83efe90dca1978a52782b9c4932
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-09-19 11:05:44 -0700

    pushing files

--- controllers/plants_controller.js
+++ controllers/plants_controller.js
@@ -20,7 +20,7 @@ plants.get('/:id', (req, res)=>{
 // INDEX
 plants.get('/', (req, res) => {
     Plants.find({}, (err, allPlants)=>{
-      res.render('index.ejs', {
+      res.render('perrenial/index.ejs', {
         plants: allPlants
     })
   })
